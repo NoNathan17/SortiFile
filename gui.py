e58a4b6c6c991ab7b332776c5715d6d09613f9cc
NoNathan17 <nathanong11217@gmail.com>
2024-07-22 22:46:15 -0700

    adjusted widgets to ttk instead of tk

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, ttk
 from pathlib import Path
 import main
 
@@ -26,28 +26,28 @@ def organize_files():
 
 root = tk.Tk()
 root.title('File Organizer')
-root.geometry('500x225')
+root.geometry('535x250')
 
 directory_path = tk.StringVar()
 sorting_method = tk.StringVar(value="extension")
 
-label = tk.Label(root, text="Select Directory:")
+label = ttk.Label(root, text="Select Directory:")
 label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 
-entry = tk.Entry(root, textvariable=directory_path, width=50)
+entry = ttk.Entry(root, textvariable=directory_path, width=50)
 entry.grid(row=0, column=1, padx=10, pady=10)
 
-browse_button = tk.Button(root, text="Browse", command=browse_directory)
+browse_button = ttk.Button(root, text="Browse", command=browse_directory)
 browse_button.grid(row=0, column=2, padx=10, pady=10)
 
-sort_by_label = tk.Label(root, text="Sort files by:")
+sort_by_label = ttk.Label(root, text="Sort files by:")
 sort_by_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
 
-sort_by_extension = tk.Radiobutton(root, text="Extension", variable=sorting_method, value="extension")
+sort_by_extension = ttk.Radiobutton(root, text="Extension", variable=sorting_method, value="extension")
 sort_by_extension.grid(row=1, column=1, padx=10, pady=10, sticky="w")
 
-sort_by_date = tk.Radiobutton(root, text="Date Created", variable=sorting_method, value="date")
+sort_by_date = ttk.Radiobutton(root, text="Date Created", variable=sorting_method, value="date")
 sort_by_date.grid(row=2, column=1, padx=10, pady=10, sticky="w")
 
-organize_button = tk.Button(root, text="Organize Files", command=organize_files)
+organize_button = ttk.Button(root, text="Organize Files", command=organize_files)
 organize_button.grid(row=3, column=0, columnspan=3, pady=20)
